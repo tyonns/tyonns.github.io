78f6deb923ea55c38b5a149ef3cf3549fda83c90
Taylor Nunes <nunes@teiranoMacBook-puro.local>
2020-09-28 22:17:13 +0900

    color: +- incrementation location: ever increasing circular radius

--- Art/artist.js
+++ Art/artist.js
@@ -2,19 +2,18 @@ class Artist {
     constructor(width, height, temperature, humidity, percipitationChance, windSpeed) {
         this.width = width;
         this.height = height;
-        this.area = this.width*this.height;
+        this.area = this.width * this.height;
         this.temperature = temperature;
         this.humidity = humidity;
         this.percipitationChance = percipitationChance;
         this.windSpeed = windSpeed;
         this.isPixelColored = new Array(this.area).fill(false);
-        this.colorRed = randomInt(255);
-        this.colorGreen = randomInt(255);
-        this.colorBlue = randomInt(255);
+        this.pixelColor = [randomInt(255), randomInt(255), randomInt(255)]; // r g b 
         this.pixelIndex = randomInt(this.area);
-        this.colorToIncrement = randomInt(2);
-        this.plusMinusVal = 2;
-        this.circleNumber = (this.plusMinusVal*2 + 1)**2 -1;
+        this.rgbIndex = randomInt(2);
+        this.colorCounter = 0;
+        this.colorIncrement = [1,1,1]; //r g b
+
 
     }
 
@@ -25,71 +24,86 @@ class Artist {
     }
 
     changePixelLocation() {
-        let [previousX, previousY] = i2xy(this.pixelIndex, this.width)
-        //console.log(this.pixelIndex);
-        let totalIncrement = 0;
-        let surroundingIndex = randomInt(this.circleNumber-1);
-        let isChangeLocation = false;
-        let newLocation;
+        const [previousX, previousY] = i2xy(this.pixelIndex, this.width)
+        if (this.isPixelColored.every(Boolean)) return;
 
+        let relativeX;
+        let relativeY;
+        let newX;
+        let newY;
+        let maxPixelAway = 1;
 
-        while (true)
-        {
-            //console.log("strt whil");
-            surroundingIndex = (surroundingIndex + totalIncrement) % this.circleNumber;
-            if (surroundingIndex >= this.circleNumber/2) surroundingIndex += 1;
-            let [deltaX, deltaY] = i2xy(surroundingIndex,(this.plusMinusVal*2+1));
-            let newX = previousX + deltaX - this.plusMinusVal;
-            let newY = previousY + deltaY - this.plusMinusVal;
-            newX = Math.max(newX,0);
-            newY = Math.max(newY,0);
-            newX = Math.min(newX,this.width);
-            newY = Math.min(newY,this.height);
-            //console.log([deltaX - 2, deltaY - 2]);
-            newLocation = xy2i([newX, newY], this.width);
-            //console.log(newLocation);
-            if (!this.isPixelColored[newLocation]){
-                break
-            }else if(totalIncrement>=this.circleNumber) {
-                newLocation = randomInt(this.width*this.height);
-                while(this.isPixelColored[newLocation]){
-                    newLocation = (newLocation + 1) % this.area;
-                }
-                isChangeLocation = true;
-                break
+        while (this.isPixelColored[this.pixelIndex]) {
+
+            let circumference = maxPixelAway * 8;
+            let circumIndex = randomInt(circumference);
+            let circumCounter = 0;
+
+            while (circumCounter < circumference && this.isPixelColored[this.pixelIndex]) {
+
+                [relativeX, relativeY] = this.convertRadInd2XY(circumIndex, maxPixelAway, circumference);
+                newX = Math.max(previousX + relativeX, 0);
+                newY = Math.max(previousY + relativeY, 0);
+                newX = Math.min(newX, this.width);
+                newY = Math.min(newY, this.height);
+                this.pixelIndex = xy2i([newX, newY], this.width);
+
+                circumIndex = (circumIndex + 1) % circumference;
+                circumCounter++;
+
             }
-            
-            totalIncrement++;
-        }
 
-        this.pixelIndex = newLocation;
-        this.isPixelColored[newLocation] = true;
-        if (isChangeLocation){
-            this.colorToIncrement = (this.colorToIncrement + 1) % 3;
-            //console.log(this.colorToIncrement)
+            console.log(maxPixelAway);
+            maxPixelAway++;
+
         }
-            
-        
     }
 
-    changePixelColor(previousRGB, index) {
-        if (this.colorToIncrement == 0){
-            this.colorRed = (this.colorRed + 1) % 256 ;
-        } else if (this.colorToIncrement == 1){
-            this.colorGreen = (this.colorGreen + 1) % 256 ;
-        } else{
-            this.colorBlue = (this.colorBlue + 1) % 256 ;
+    convertRadInd2XY(index, maxPixelAway, totalPixels) {
+        let diameter = maxPixelAway * 2 + 1;
+        let relativeY;
+        let relativeX;
+        if (index < diameter) {
+            relativeY = - maxPixelAway;
+            relativeX = index - maxPixelAway;
+        } else if (index < (totalPixels - diameter)) {
+            relativeX = maxPixelAway * (-1) ** index;
+            relativeY = Math.floor((index - diameter) / 2) - maxPixelAway + 1;
+        } else if (index < totalPixels) {
+            relativeY = maxPixelAway;
+            relativeX = index - totalPixels + maxPixelAway + 1;
         }
-        console.log([this.colorRed,this.colorGreen,this.colorBlue]);
+        //console.log(maxPixelAway);
+        //console.log(index);
+        //console.log([relativeX, relativeY]);
+        return [relativeX, relativeY];
+    }
+
+    changePixelColor(){
         
+        let changeValue = 4;
+        this.pixelColor[this.rgbIndex] = this.pixelColor[this.rgbIndex] + this.colorIncrement[this.rgbIndex];
+        if (this.pixelColor[this.rgbIndex]>=256 || this.pixelColor[this.rgbIndex]<=0)
+        {
+            this.colorIncrement[this.rgbIndex] *= -1;
+            this.pixelColor[(this.rgbIndex + 1)%3] += changeValue * this.colorIncrement[(this.rgbIndex + 1)%3] ;
+            this.pixelColor[(this.rgbIndex + 2)%3] += changeValue * this.colorIncrement[(this.rgbIndex + 2)%3] ;
+            if (this.pixelColor[(this.rgbIndex + 1)%3]>=256 || this.pixelColor[(this.rgbIndex + 1)%3]<=0){
+                this.colorIncrement[(this.rgbIndex + 1)%3] *= -1;
+            }
+            if (this.pixelColor[(this.rgbIndex + 2)%3]>=256 || this.pixelColor[(this.rgbIndex + 2)%3]<=0){
+                this.colorIncrement[(this.rgbIndex + 2)%3] *= -1;
+            }
+        }
 
     }
 
-    drawPixel(fieldImgData){
-        //console.log(this.pixelIndex);
-        fieldImgData.data[this.pixelIndex*4+0]=this.colorRed;
-        fieldImgData.data[this.pixelIndex*4+1]=this.colorGreen;
-        fieldImgData.data[this.pixelIndex*4+2]=this.colorBlue;
+    drawPixel(fieldImgData) {
+        console.log(this.pixelIndex);
+        fieldImgData.data[this.pixelIndex * 4 + 0] = this.pixelColor[0];
+        fieldImgData.data[this.pixelIndex * 4 + 1] = this.pixelColor[1];
+        fieldImgData.data[this.pixelIndex * 4 + 2] = this.pixelColor[2];
+        this.isPixelColored[this.pixelIndex] = true;
     }
 
 }
